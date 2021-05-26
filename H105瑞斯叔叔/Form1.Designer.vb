c9d1a6003294748995e3915e2641a5d2461d5a45
dzif22ndproject <84844026+dzif22ndproject@users.noreply.github.com>
2021-05-26 18:07:29 +0800

    Add files via upload

---
+++ H105瑞斯叔叔/Form1.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(1151, 484)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(189, 102)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "開剁"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Location = New System.Drawing.Point(1237, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(103, 67)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "先別剁"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.H105瑞斯叔叔.My.Resources.Resources.齁齁齁齁齁
+        Me.PictureBox4.Location = New System.Drawing.Point(844, 331)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(200, 150)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.H105瑞斯叔叔.My.Resources.Resources.咚
+        Me.PictureBox3.Location = New System.Drawing.Point(323, 141)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(200, 150)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.H105瑞斯叔叔.My.Resources.Resources.AAAAAAAAAAA
+        Me.PictureBox2.Location = New System.Drawing.Point(857, 92)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(200, 150)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.H105瑞斯叔叔.My.Resources.Resources.咚咚
+        Me.PictureBox1.Location = New System.Drawing.Point(335, 345)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(200, 150)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("微軟正黑體", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(157, 35)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "剁嬰兒次數:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("微軟正黑體", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(166, 11)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 37)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "0"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 26)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "時間:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("微軟正黑體", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label4.Location = New System.Drawing.Point(89, 62)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 61)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "30"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label5.Location = New System.Drawing.Point(577, 286)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(185, 26)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "你剁的嬰兒個數為:"
+        Me.Label5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("微軟正黑體", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label6.Location = New System.Drawing.Point(768, 286)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(25, 26)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "0"
+        Me.Label6.Visible = False
+        '
+        'Timer3
+        '
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 3000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1352, 652)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+End Class
---
+++ H105瑞斯叔叔/Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports WMPLib
+
+Public Class Form1
+    Private player As New WMPLib.WindowsMediaPlayer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '開剁
+        Button1.Enabled = False
+        Button1.Visible = False
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+        Label4.Text = 30
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '先別剁
+        Button1.Enabled = True
+        Button1.Visible = True
+        PictureBox1.Visible = False
+        PictureBox2.Visible = False
+        PictureBox3.Visible = False
+        PictureBox4.Visible = False
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        Label2.Text = 0
+        Label4.Text = 30
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        '國動亂跳
+        PictureBox1.Visible = True
+        PictureBox2.Visible = True
+        PictureBox3.Visible = True
+        PictureBox4.Visible = True
+        Randomize()
+        PictureBox1.Left = Int(20 + Rnd() * (Me.Width - 40 + PictureBox1.Left - 0 + 1))
+        Randomize()
+        PictureBox2.Left = Int(20 + Rnd() * (Me.Width - 40 + PictureBox1.Left - 0 + 1))
+        Randomize()
+        PictureBox3.Left = Int(20 + Rnd() * (Me.Width - 40 + PictureBox1.Left - 0 + 1))
+        Randomize()
+        PictureBox4.Left = Int(20 + Rnd() * (Me.Width - 40 + PictureBox1.Left - 0 + 1))
+        PictureBox1.Enabled = True
+        PictureBox2.Enabled = True
+        PictureBox3.Enabled = True
+        PictureBox4.Enabled = True
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        '國動加分
+        Label2.Text += 1
+        PictureBox1.Enabled = False
+        PictureBox1.Visible = False
+    End Sub
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        '國動加分
+        Label2.Text += 1
+        PictureBox2.Enabled = False
+        PictureBox2.Visible = False
+    End Sub
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBox3.Click
+        '國動加分
+        Label2.Text += 1
+        PictureBox3.Enabled = False
+        PictureBox3.Visible = False
+    End Sub
+    Private Sub PictureBox4_Click(sender As Object, e As EventArgs) Handles PictureBox4.Click
+        '國動加分
+        Label2.Text += 1
+        PictureBox4.Enabled = False
+        PictureBox4.Visible = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        '時間倒數
+        Label4.Text -= 1
+        If Label4.Text < 0 Then   '當時間結束
+            Button1.Enabled = True
+            Button1.Visible = True
+            PictureBox1.Visible = False
+            PictureBox2.Visible = False
+            PictureBox3.Visible = False
+            PictureBox4.Visible = False
+            Timer1.Enabled = False
+            Timer2.Enabled = False
+            Label4.Text = 30
+            Timer3.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        '宣告成績
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Label5.Visible = True
+        Label6.Visible = True
+        Label6.Text = Label2.Text
+        Label2.Text = 0
+        Timer3.Enabled = False
+        Timer4.Enabled = True
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        Label5.Visible = False
+        Label6.Visible = False
+        Button1.Enabled = True
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        player.URL = My.Application.Info.DirectoryPath & "\BackMusic123.wav"
+        player.settings.setMode("loop", True)
+        player.settings.volume = 5
+    End Sub
+End Class
